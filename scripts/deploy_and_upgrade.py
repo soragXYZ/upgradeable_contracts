832baf0cb5fbb0b034de7d5c348cdc73dc232a8e
sorag <sorag.eth@protonmail.com>
2021-10-22 15:25:36 +0200

    Initial commit

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,38 @@
+from scripts.helpful_scripts import get_account, encode_function_data, upgrade
+from brownie import config, network, Box, BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account}, publish_source=config["networks"][network.show_active()]["verify"])
+    print(box.retrieve())
+
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=config["networks"][network.show_active()]["verify"])
+
+    initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+        publish_source=config["networks"][network.show_active()]["verify"]
+    )
+    print(f"Proxy deployed to {proxy}, can now upgradde to v2")
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    
+    box_v2 = BoxV2.deploy({"from": account}, publish_source=config["networks"][network.show_active()]["verify"])
+    upgrade_transaction = upgrade(
+        account,
+        proxy,
+        box_v2.address,
+        proxy_admin_contract= proxy_admin
+    )
+    upgrade_transaction.wait(1)
+    print("Proxy has been upgraded")
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    tx = proxy_box.increment({"from": account})
+    tx.wait(1)
+    print(proxy_box.retrieve())
+
